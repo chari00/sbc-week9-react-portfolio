034961d0483c73f3ee3dcd4fc479e7550ed3d12b
chari00 <macharitocruzuk@gmail.com>
2024-11-18 11:42:04 +0000

    push starter file

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+//this is serves as the home page
+  return (
+    <>
+      <div>this.App.jsx</div>
+      <div>header<span> ➡️ the header will have the navigation bar</span></div>
+      <div>About</div>
+      <div>Projects / gallery</div>
+      <div>Experience</div>
+      <div>Contact <span> ➡️ Contact form</span></div>
+      <div>footer</div>
+
+      
+    </>
+  )
+}
+
+export default App
