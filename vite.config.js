6c4e666f593b69638f206c4e80cf8f47b6b12d40
chari00 <macharitocruzuk@gmail.com>
2024-11-26 10:33:38 +0000

    configure vite config file for project optimization, move clsx, tailwind-merge and @types/three to devdependencies.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig, splitVendorChunkPlugin  } from 'vite'
+import { defineConfig  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react() ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui': ['@material-tailwind/react', '@heroicons/react'],
+          'utils': ['axios', 'framer-motion', 'simplex-noise'],
       }
     }
+  }
   }
 })
